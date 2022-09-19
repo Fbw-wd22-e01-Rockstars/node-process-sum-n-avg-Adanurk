f43ad14cb44bc74cc0a7065337f97e7bd76452ae
Adanurk <gumussoykbr@gmail.com>
2022-09-19 15:17:01 +0200

    edit

--- solution.js
+++ solution.js
@@ -1,26 +1,5 @@
 const argvs = process.argv.slice(2);
 
-// const program = (op, arr) => {
-//     let myArr = arr.map(num => +num);
-//     myArr.forEach(element  => {
-//         if(Number.isNaN(element)){
-//             return `${element} is not a number, try again`
-//         }});
-//         switch (op) {
-//             case "sum":
-//                 myArr.reduce((a,b) => a+b,0)
-//             break;
-//             case "avg":
-//                 myArr.reduce((a,b) => a+b)/arr.length;
-//             break;
-//             default:
-//                 console.log("I cannot calculate that, please type either 'sum' (to calculate the sum) or 'avg' (to calculate the Average)");
-//                 break;
-//         }
-// }
-
-// console.log(program(argvs[0], argvs.slice(1)));
-
 const myFunction = (op, arr) => {
     let x;
     for (let i = 0; i < arr.length; i++) {
